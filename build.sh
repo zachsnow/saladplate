39e6e97c9089769f208babe2ce972ea3e4ecf7a7
Zach Snow <z@zachsnow.com>
2024-05-15 16:13:57 -0700

    Update to a normal version number.

--- build.sh
+++ build.sh
@@ -1,2 +1,7 @@
+# Build.
 pnpm exec tsc
+
+# The shebang lets us run the script directly as TypeScript
+# via `ts-node`, but we need to change it to use `node` in
+# the compiled JS.
 sed -i '' '1 s/ts-node/node/' ./dist/bin/saladplate.js
